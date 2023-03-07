cb0773ea15b44e0a8487aa640f4a75122d832f6e
Michael Gastello <michaelgastello@aol.com>
2023-03-07 15:39:59 -0500

    working on bindevents

--- skeleton 4/src/ttt-view.js
+++ skeleton 4/src/ttt-view.js
@@ -8,6 +8,7 @@ class View {
     const grid = document.createElement('ul')
     for (let i = 0; i < 9; i++) {
       let li = document.createElement('li')
+      li.classList.add('unclicked')
       grid.appendChild(li)
     }
     return grid
@@ -15,9 +16,7 @@ class View {
   
   bindEvents() {}
 
-  handleClick(e) {
-    const space = document.getElementById
-  }
+  handleClick(e) {}
 
   makeMove(square) {}
 
